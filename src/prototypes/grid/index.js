e75bb6e6529666575e751c57e132f8efffba806a
Brandon Mellus <mellusbrandon@gmail.com>
2018-11-07 15:38:08 -0500

    Work on Grid prototype

--- src/prototypes/grid/index.js
+++ src/prototypes/grid/index.js
@@ -1,6 +1,4 @@
-function getLiveCellKey(x, y) {
-  return `${x},${y}`;
-}
+const getCellKey = (x, y) => `${x},${y}`;
 
 export default class Grid {
   constructor(x, y) {
@@ -13,15 +11,89 @@ export default class Grid {
 
   addLiveCell(x, y) {
     this._grid[y][x] = 1;
-    this._liveCells[getLiveCellKey(x, y)] = true;
+    this._liveCells[getCellKey(x, y)] = true;
   }
 
   killCell(x, y) {
     this._grid[y][x] = 0;
-    delete this._liveCells[getLiveCellKey(x, y)];
+    delete this._liveCells[getCellKey(x, y)];
+  }
+
+  getNeighbors(x, y) {
+    return {
+      ...this._getUpperLeftNeighbor(x, y),
+      ...this._getUpperMiddleNeighbor(x, y),
+      ...this._getUpperRightNeighbor(x, y),
+      ...this._getLeftNeighbor(x, y),
+      ...this._getRightNeighbor(x, y),
+      ...this._getBottomLeftNeighbor(x, y),
+      ...this._getBottomMiddleNeighbor(x, y),
+      ...this._getBottomRightNeighbor(x, y),
+    };
+  }
+
+  countNeighbors(x, y) {
+    const neighbors = this.getNeighbors(x, y);
+    return Object.keys(neighbors).reduce((curTotal, key) => curTotal + neighbors[key], 0);
   }
 
   get grid() {
     return this._grid;
+  }
+
+  get horizontalLength() {
+    return this._grid[0].length;
+  }
+
+  get verticalLength() {
+    return this._grid.length;
+  }
+
+  _inBounds(x, y) {
+    return this._inXBounds(x) && this._inYBounds(y);
+  }
+
+  _inXBounds(x) {
+    return x >= 0 && x < this.horizontalLength;
+  }
+
+  _inYBounds(y) {
+    return y >= 0 && y < this.verticalLength;
+  }
+
+  _getANeighbor(x, y) {
+    return { [getCellKey(x, y)]: this._inBounds(x, y) ? this._grid[y][x] : null };
+  }
+
+  _getUpperLeftNeighbor(x, y) {
+    return this._getANeighbor(x-1, y-1);
+  }
+
+  _getUpperMiddleNeighbor(x, y) {
+    return this._getANeighbor(x, y-1);
+  }
+
+  _getUpperRightNeighbor(x, y) {
+    return this._getANeighbor(x+1, y-1);
+  }
+
+  _getLeftNeighbor(x, y) {
+    return this._getANeighbor(x-1, y);
+  }
+
+  _getRightNeighbor(x, y) {
+    return this._getANeighbor(x+1, y);
+  }
+
+  _getBottomLeftNeighbor(x, y) {
+    return this._getANeighbor(x-1, y+1);
+  }
+
+  _getBottomMiddleNeighbor(x, y) {
+    return this._getANeighbor(x, y+1);
+  }
+
+  _getBottomRightNeighbor(x, y) {
+    return this._getANeighbor(x+1, y+1);
   }
 }
--- src/prototypes/grid/index.test.js
+++ src/prototypes/grid/index.test.js
@@ -8,6 +8,7 @@ describe('Grid', () => {
       [0, 0, 0, 0],
     ]);
   });
+
   it('generates a grid', () => {
     expect(new Grid(7, 7).grid).toEqual([
       [0, 0, 0, 0, 0, 0, 0],
@@ -19,6 +20,7 @@ describe('Grid', () => {
       [0, 0, 0, 0, 0, 0, 0],
     ])
   });
+
   it('adds a live to cell to coordinate', () => {
     const grid = new Grid(4, 3);
     grid.addLiveCell(2, 2);
@@ -28,6 +30,7 @@ describe('Grid', () => {
       [0, 0, 1, 0],
     ]);
   });
+
   it('kills a cell at a coordinate', () => {
     const grid = new Grid(4, 3);
     grid.addLiveCell(1, 1);
@@ -38,11 +41,57 @@ describe('Grid', () => {
       [0, 0, 0, 0],
     ]);
   });
+
   it('gets the neighbors of cell', () => {
     const grid = new Grid(5, 5);
-    grid.addLiveCell(2, 2);
-    expect(grid.getNeighbors(2, 2)).toEqual([
-      
-    ]);
+    expect(grid.getNeighbors(2, 2)).toEqual({
+      '1,1': 0,
+      '2,1': 0,
+      '3,1': 0,
+      '1,2': 0,
+      '3,2': 0,
+      '1,3': 0,
+      '2,3': 0,
+      '3,3': 0,
+    });
+  });
+
+  it('gets the neighbors of cell', () => {
+    const grid = new Grid(10, 6);
+    expect(grid.getNeighbors(4, 3)).toEqual({
+      '3,2': 0,
+      '4,2': 0,
+      '5,2': 0,
+      '3,3': 0,
+      '5,3': 0,
+      '3,4': 0,
+      '4,4': 0,
+      '5,4': 0,
+    });
+  });
+
+  it('marks `null` for no neighbor', () => {
+    const grid = new Grid(1, 1);
+    expect(grid.getNeighbors(0, 0)).toEqual({
+      '-1,-1': null,
+      '0,-1': null,
+      '1,-1': null,
+      '-1,0': null,
+      '1,0': null,
+      '-1,1': null,
+      '0,1': null,
+      '1,1': null,
+    });
+  });
+
+  it('counts the neighbors around a cell', () => {
+    const grid = new Grid(4, 3);
+    expect(grid.countNeighbors(2,2)).toEqual(0);
+  });
+
+  it('counts the neighbors around a cell', () => {
+    const grid = new Grid(4, 3);
+    grid.addLiveCell(1,2);
+    expect(grid.countNeighbors(2,2)).toEqual(1);
   });
 });
